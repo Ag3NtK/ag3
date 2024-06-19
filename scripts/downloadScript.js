562f3a44771f9c01c5e5a3aeb59d6e6c118615c7
Javier Pedrajas Mendoza <101878271+Ag3NtK@users.noreply.github.com>
2024-06-19 19:17:08 +0200

    Create downloadScript.js

---
+++ scripts/downloadScript.js
@@ -0,0 +1,17 @@
+// script.js
+
+function downloadFile(url, filename) {
+    // Create a temporary link element
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+
+    // Append the link to the body
+    document.body.appendChild(link);
+
+    // Programmatically click the link to trigger the download
+    link.click();
+
+    // Remove the link from the document
+    document.body.removeChild(link);
+}
